93f73536a0f77978c10a7e1a8d4809a4f4fa6472
HenryVexZHou <43188301+NeverLucky123@users.noreply.github.com>
2020-04-27 22:16:37 -0700

    Added CLI interface

--- venv/Include/Transpiler.py
+++ venv/Include/Transpiler.py
@@ -2,6 +2,7 @@
 from File import *
 import os
 import subprocess
+import argparse
 
 
 # x = File(os.getcwd() + "/sample_discord.dis")
@@ -21,7 +22,7 @@ class project:
         self.output_exe = output_exe
         if not include_dir:
             include_dir = [os.path.dirname(self.root)]
-        include_dir.append(os.getcwd())
+        include_dir.append(os.path.abspath(os.path.dirname(__file__)))
         self.include_dirs = include_dir
         self.files = []
 
@@ -33,12 +34,16 @@ class project:
     def transpile(self, address=None):
         if not address:
             address = self.root
+        if os.path.isfile(os.path.splitext(address)[0]+".h"):
+            os.remove(os.path.splitext(address)[0]+".h")
+        if os.path.isfile(os.path.splitext(address)[0] + ".cpp"):
+            os.remove(os.path.splitext(address)[0] + ".cpp")
         file = File(address)
         file.process()
         # if compiling a .dis file
         if address.split(".")[-1] == "dis":
             file.sync(address + "_")
-            subprocess.call([os.path.join(os.getcwd(), "lazycpp.exe"),
+            subprocess.call([os.path.join(os.path.abspath(os.path.dirname(__file__)), "lazycpp.exe"),
                              "-o", os.path.dirname(address),
                              os.path.normpath(address + "_")], shell=True)
             os.remove(address + "_")
@@ -49,20 +54,31 @@ class project:
             self.compile(find(include))
 
     def build(self):
-        files = "".join(self.files)
+        if os.path.isfile(self.output_exe):
+            os.remove(self.output_exe)
         gcc_I = []
         for dir in self.include_dirs:
             gcc_I.append("-I " + dir + " ")
         subprocess.call(["g++", self.files,
                          "-o", self.output_exe,
-                         gcc_I], shell=True)
+                        gcc_I], shell=True)
 
     def run(self):
         subprocess.call([self.output_exe])
 
-a = project("C:/Users/henry/PycharmProjects/Discordant_Transpiler/venv/Include/test_proj/sample_discord.dis")
+
+parser = argparse.ArgumentParser(description='Transpile, Compile, or Run Discordant Source Code')
+parser.add_argument("-i", "-include", nargs="*")
+parser.add_argument("action", choices=['transpile', 'make', 'run'])
+parser.add_argument("infile")
+parser.add_argument('outfile', nargs='?')
+#args = parser.parse_args(["make", "test_proj/sample_discord.dis", "-i", "asd"])
+args = parser.parse_args()
+a = project(os.path.abspath(args.infile), args.i, args.outfile)
+
 a.transpile()
-a.build()
-a.run()
-# print(a.root)
-# print(a.include_dirs)
+if args.action == "make":
+    a.build()
+elif args.action == "run":
+    a.build()
+    a.run()
